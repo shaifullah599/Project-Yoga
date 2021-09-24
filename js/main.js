8ffab76771603248b8f15fc6d847de9bebb1ba63
shaifullah599 <83369080+shaifullah599@users.noreply.github.com>
2021-09-24 09:12:30 -0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// banner slick slider
+
+
+$('.banner').slick({
+  prevArrow: '<i class="fas fa-chevron-left"></i>' ,
+  nextArrow: '<i class="fas fa-chevron-right"></i>',
+  dots: true,  
+});
+
+
+
+// testimonial
+
+$('.testimonial-slider').slick({
+
+  dots: true,  
+  arrows: false
+});
+
+
+
